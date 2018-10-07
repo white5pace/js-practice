d3256682fe35e2ace12306ae956f3ad7feb5a7e5
white5pace <kuchmin_roman@mail.ru>
2018-10-07 06:01:05 +0300

    End of circle app

--- circle/script.js
+++ circle/script.js
@@ -1,41 +1,68 @@
 
 	var circle = document.querySelector('.circle');
-	var dot = document.querySelector('.dot');
+	var dot = document.querySelector('#dot-iput');
+	var dotState = document.querySelector('.dot-state h1');	
+	var r = 0;
 
 	function showCircle(){
 		var circleValue = document.querySelector('#circle-value').value*2;
 		circle.style.width = circleValue * 10 + 'px';
 		circle.style.height = circleValue * 10 + 'px';
+		r = parseInt(circle.style.width)/2;
+
 	}
 
 	function showDot(){
-		var xValue = document.querySelector('#x-value').value;
-		var yValue = document.querySelector('#y-value').value;
-		var r = parseInt(circle.style.width)/2;
-		var dotState = document.querySelector('.dot-state h1');
+		var xInputValue = document.querySelector('#x-value').value;
+		var yInputValue = document.querySelector('#y-value').value;
+		var xValue	= xInputValue *10;
+		var yValue	= yInputValue *10;
 		dot.style = '';
-		dotState.innerHTML = '';
 
 		if(xValue > 0){
-			dot.style.left = r + xValue*10 - 5 + 'px';
-		}
-		else{
-			dot.style.right = r + xValue*-10 - 5 + 'px';
+			dot.style.left = r + xValue - 5 + 'px';
+		}else{
+			dot.style.right = r + xValue*-1 - 5 + 'px';
 		}
 
 		if(yValue > 0){
-			dot.style.bottom = r + yValue*10 - 5 + 'px';
+			dot.style.bottom = r + yValue - 5 + 'px';
+		}else{
+			dot.style.top = r+ yValue*-1 - 5 + 'px';
 		}
-		else{
-			dot.style.top = r + yValue*-10 - 5+ 'px';
-		}
-		var hypotenuse = Math.sqrt(xValue*xValue + yValue*yValue);
-		if(hypotenuse*10 < r){
+
+		isBelongs(xValue, yValue);
+		dot.style.display = 'inline-block';
+
+	}
+	function isBelongs(x, y){
+
+		dotState.innerHTML = '';
+		var hypotenuse = Math.sqrt(x*x + y*y);
+		if(hypotenuse < r){
 			dotState.innerHTML = 'Точка принадлежит кругу';
 		}else{
 			dotState.innerHTML = 'Точка не принадлежит кругу';
 		}
-		dot.style.display = 'inline-block';
-
 	}
+
+	document.querySelector('.objects').onclick = function(e) {
+		dot = document.querySelector('#dot-mouse');
+		var x = e.offsetX==undefined?e.layerX:e.offsetX;
+		var y = e.offsetY==undefined?e.layerY:e.offsetY;
+		dot.style.top = y + 'px';
+		dot.style.left = x + 'px';
+		dot.style.display = 'inline-block';
+	}
+
+	document.querySelector('.objects').addEventListener('click', function(e) {
+		var	click = document.elementFromPoint(e.clientX, e.clientY).className;
+		console.log(click);
+		if(click == 'circle'){
+			dotState.innerHTML = 'Точка принадлежит кругу';
+		}else{
+			dotState.innerHTML = 'Точка не принадлежит кругу';
+		}
+
+	}, false);
 
