8a9d586b471ae285f1edb9c5275f26724d1c638e
white5pace <kuchmin_roman@mail.ru>
2019-05-17 17:51:13 +0300

    notebook commit

--- LJS7/3/script.js
+++ LJS7/3/script.js
@@ -35,17 +35,17 @@ var writeTo = document.getElementById('outputvalue');
 
 // 3 Функция-задержка
 
-
 function f(x) {
-  var delay = setTimeout(function delay(){
-
-  },)
+  console.log(x);
 }
 
 var f1000 = delay(f, 1000);
-var f1500 = delay(f, 1500);
+// var f1500 = delay(f, 1500);
 
 f1000("тест"); // выведет "тест" через 1000 миллисекунд
-f1500("тест2"); // выведет "тест2" через 1500 миллисекунд
+// f1500("тест2"); // выведет "тест2" через 1500 миллисекундfunction f(x) 
 
+var delay = setTimeout(f, x);
+
+f(5);
 
