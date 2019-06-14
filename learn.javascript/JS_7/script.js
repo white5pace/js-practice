7832ccc06c81a15e7f228440c907a73086ffa705
white5pace <kuchmin_roman@mail.ru>
2019-06-14 04:05:09 +0300

    7.2

--- learn.javascript/JS_7/script.js
+++ learn.javascript/JS_7/script.js
@@ -47,3 +47,43 @@ function exOne() {
 
   htmlOut('Полиморфная функция formatDate', exOne.toString(),formatDate(new Date(2014, 0, 1)), 'https://learn.javascript.ru/class-instanceof#polimorfnaya-funktsiya-formatdate' );
 }
+
+// 2 Превратите объект в JSON
+
+function exTwo() {
+  var leader = {
+    name: "Василий Иванович",
+    age: 35
+  };
+  leader = JSON.stringify(leader);
+  leader = JSON.parse(leader);
+
+  htmlOut('Превратите объект в JSON', exTwo.toString(),leader.age, 'https://learn.javascript.ru/json#prevratite-obekt-v-json' );
+}
+
+
+// 3 Превратите объекты со ссылками в JSON
+
+function exThree() {
+  var leader = {
+    name: "Василий Иванович"
+  };
+  
+  var soldier = {
+    name: "Петька"
+  };
+  
+  // эти объекты ссылаются друг на друга!
+  leader.soldier = soldier;
+  soldier.leader = leader;
+  
+  var team = [leader, soldier];
+  var str = JSON.stringify(team, function(key, value) {
+    if (key == 'leader' || key == 'soldier') return value.name     ;
+    return value;
+  });
+  console.log(str);
+
+  htmlOut('Превратите объекты со ссылками в JSON', exThree.toString(),str, 'https://learn.javascript.ru/json#prevratite-obekty-so-ssylkami-v-json' );
+}
+exThree();
