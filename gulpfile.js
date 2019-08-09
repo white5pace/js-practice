ed5bafd5a31d0c2c966df9569be6878ecaa5e328
white5pace <kuchmin_roman@mail.ru>
2019-08-09 17:47:26 +0300

    change dir name

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var gulp         = require('gulp'),
 gulp.task('browser-sync', function(){
   browserSync({
     server: {
-      baseDir: 'learn.javascript'
+      baseDir: 'learn-javascript'
     },
     notify: false
   })
 });
 
 gulp.task('watch', function(){
-  gulp.watch('learn.javascript/*.html', browserSync.reload());
-  gulp.watch('learn.javascript/**/*.js', browserSync.reload());
+  gulp.watch('learn-javascript/*.html', browserSync.reload());
+  gulp.watch('learn-javascript/**/*.js', browserSync.reload());
 });
 gulp.task('default', gulp.parallel('watch', 'browser-sync'))
