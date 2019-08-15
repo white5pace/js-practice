de0799e89e6b274c7a1b9fb9f5eb1eb599744bfa
white5pace <kuchmin_roman@mail.ru>
2019-08-15 18:50:49 +0300

    5.5, styles to list-sub

--- learn-javascript/UJS_5/script.js
+++ learn-javascript/UJS_5/script.js
@@ -206,10 +206,285 @@ function exTen() {
 }
 
 // 5.4.5 Подмассив наибольшей суммы
+
 function exEleven() {
   htmlOut( 'Подмассив наибольшей суммы',
       exEleven.toString(),
       'Еще не решил',
       'https://learn.javascript.ru/task/maximal-subarray'
+  );
+}
+
+// 5.5.1 Переведите текст вида border-left-width в borderLeftWidth
+
+function exTwelve() {
+  let camelize = (str) => {
+    let splited = str.split('-');
+    for (let i = 1; i < splited.length; i++) {
+      splited[i] = splited[i][0].toUpperCase() + splited[i].slice(1);
+    }
+    return splited.join('');
+  };
+
+  console.log(camelize('background-color'));
+  console.log(camelize('list-style-image'));
+  console.log(camelize('-webkit-transition'));
+
+  htmlOut( 'Переведите текст вида border-left-width в borderLeftWidth',
+      exTwelve.toString(),
+      camelize('background-color'),
+      'https://learn.javascript.ru/task/camelcase'
+  );
+}
+
+// 5.5.2 Фильтрация по диапазону
+
+function exThirteen() {
+  let arr = [5, 3, 8, 1];
+  function filterRange(arr, from, to) {
+    return arr.filter((item) => item >= from && item <= to);
+  }
+  let filtered = filterRange(arr, 1, 4);
+  console.log( filtered ); // 3,1 (совпадающие значения)
+  console.log( arr ); // 5,3,8,1 (без изменений)
+
+  htmlOut( 'Фильтрация по диапазону',
+      exThirteen.toString(),
+      filtered,
+      'https://learn.javascript.ru/task/filter-range'
+  );
+}
+
+// 5.5.2 Фильтрация по диапазону "на месте"
+
+function exFourteen() {
+  function filterRangeInPlace(arr, a, b) {
+    let i = 0;
+    while (i < arr.length) {
+      if (arr[i] < a || arr[i] > b) {
+        arr.splice(i, 1);
+      } else {
+        i++;
+      }
+    }
+  }
+  let arr = [5, 3, 8, 1];
+  filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+  console.log( arr ); // [3, 1]
+  htmlOut( 'Фильтрация по диапазону "на месте"',
+      exFourteen.toString(),
+      arr,
+      'https://learn.javascript.ru/task/filter-range-in-place'
+  );
+}
+
+// 5.5.4 Сортировать в обратном порядке
+
+function exFifteen() {
+  let arr = [5, 2, 1, -10, 8];
+  arr.sort((a, b) => b - a);
+  console.log( arr ); // 8, 5, 2, 1, -10
+  htmlOut( 'Сортировать в обратном порядке',
+      exFifteen.toString(),
+      true,
+      'https://learn.javascript.ru/task/sort-back'
+  );
+}
+
+// 5.5.5 Скопировать и отсортировать массив
+
+function exSixteen() {
+  let arr = ['HTML', 'JavaScript', 'CSS'];
+  function copySorted(arr) {
+    return arr.concat().sort();
+  }
+  let sorted = copySorted(arr);
+
+  console.log( sorted ); // CSS, HTML, JavaScript
+  console.log( arr ); // HTML, JavaScript, CSS (без изменений)
+
+  htmlOut( 'Скопировать и отсортировать массив',
+      exSixteen.toString(),
+      sorted,
+      'https://learn.javascript.ru/task/copy-sort-array'
+  );
+}
+
+// 5.5.6 Создать расширяемый калькулятор
+
+function exSeventeen() {
+  function Calculator() {
+    this.calculate = (str) => {
+      let splited = str.split(' ');
+      this.a = +splited[0];
+      this.b = +splited[2];
+      if (!this[splited[1]] || isNaN(this.a) || isNaN(this.b)) {
+        return NaN;
+      }
+      return this[splited[1]](this.a, this.b);
+    };
+
+    this.addMethod = function(method, func) {
+      this[method] = func;
+    };
+
+    this['+'] = (a, b) => a + b;
+    this['-'] = (a, b) => a - b;
+  }
+  let calc = new Calculator;
+  console.log( calc.calculate('3 + 7') ); // 10
+
+  let powerCalc = new Calculator;
+
+  powerCalc.addMethod('*', (a, b) => a * b);
+  powerCalc.addMethod('/', (a, b) => a / b);
+  powerCalc.addMethod('**', (a, b) => a ** b);
+
+  let result = powerCalc.calculate('2 ** 3');
+  console.log( result ); // 8
+
+  htmlOut( 'Создать расширяемый калькулятор',
+      exSeventeen.toString(),
+      `powerCalc.calculate('2 ** 3') = ${result}`,
+      'https://learn.javascript.ru/task/calculator-extendable'
+  );
+}
+
+// 5.5.7 Трансформировать в массив имён
+
+function exEighteen() {
+  let vasya = {name: 'Вася', age: 25};
+  let petya = {name: 'Петя', age: 30};
+  let masha = {name: 'Маша', age: 28};
+
+  let users = [vasya, petya, masha];
+
+  let names = users.map((item) => item.name);
+
+  console.log( names ); // Вася, Петя, Маша
+  htmlOut( 'Трансформировать в массив имён',
+      exEighteen.toString(),
+      names,
+      'https://learn.javascript.ru/task/array-get-names'
+  );
+}
+
+// 5.5.8 Трансформировать в объекты
+
+function exNineteen() {
+  let vasya = {name: 'Вася', surname: 'Пупкин', id: 1};
+  let petya = {name: 'Петя', surname: 'Иванов', id: 2};
+  let masha = {name: 'Маша', surname: 'Петрова', id: 3};
+
+  let users = [vasya, petya, masha];
+
+
+  let usersMapped = users.map((item) => new UserMapped(item));
+
+  function UserMapped(item) {
+    this.id = item.id;
+    this.fullName = `${item.name} ${item.surname}`;
+  };
+
+  console.log(usersMapped);
+  console.log( usersMapped[0].id ); // 1
+  console.log( usersMapped[0].fullName ); // Вася Пупкин
+  htmlOut( 'Трансформировать в объекты',
+      exNineteen.toString(),
+      `usersMapped[0].fullName = ${usersMapped[0].fullName} `,
+      'https://learn.javascript.ru/task/map-objects'
+  );
+}
+
+// 5.5.9 Отсортировать пользователей по возрасту
+
+function exTwenty() {
+  let vasya = {name: 'Вася', age: 25};
+  let petya = {name: 'Петя', age: 30};
+  let masha = {name: 'Маша', age: 28};
+
+  let arr = [vasya, petya, masha];
+
+  function sortByAge(arr) {
+    arr.sort((a, b) => a.age - b.age);
+  }
+
+  sortByAge(arr);
+
+  console.log(arr[0].name); // Вася
+  console.log(arr[1].name); // Маша
+  console.log(arr[2].name); // Петя
+  htmlOut( 'Отсортировать пользователей по возрасту',
+      exTwenty.toString(),
+      arr,
+      'https://learn.javascript.ru/task/sort-objects'
+  );
+}
+
+// 5.5.10 Перемешайте массив
+
+function exTwentyOne() {
+  let arr = [1, 2, 3];
+  function shuffle(arr) {
+    arr.sort(() => Math.random() - 0.5);
+  }
+  shuffle(arr);
+  console.log(arr);
+
+  shuffle(arr);
+  console.log(arr);
+
+  shuffle(arr);
+  console.log(arr);
+
+  htmlOut( 'Перемешайте массив',
+      exTwentyOne.toString(),
+      arr,
+      'https://learn.javascript.ru/task/shuffle'
+  );
+}
+
+// 5.5.11 Получить средний возраст
+
+function exTwentyTwo() {
+  let vasya = {name: 'Вася', age: 25};
+  let petya = {name: 'Петя', age: 30};
+  let masha = {name: 'Маша', age: 29};
+
+  let arr = [vasya, petya, masha];
+  let getAverageAge = (arr) => {
+    return arr.reduce((previousValue, item) => previousValue + item.age, 0) /
+      arr.length;
+  };
+  console.log(getAverageAge(arr));
+  htmlOut( 'Получить средний возраст',
+      exTwentyTwo.toString(),
+      getAverageAge(arr),
+      'https://learn.javascript.ru/task/average-age'
+  );
+}
+
+// 5.5.12 Оставить уникальные элементы массива
+
+function exTwentyThree() {
+  function unique(arr) {
+    let uniqueArr = [];
+    strings.forEach(function(item) {
+      if (!(uniqueArr.includes(item))) {
+        uniqueArr.push(item);
+      }
+    });
+    return uniqueArr;
+  }
+
+  let strings = ['кришна', 'кришна', 'харе', 'харе',
+    'харе', 'харе', 'кришна', 'кришна', ':-O',
+  ];
+
+  console.log( unique(strings) );
+  htmlOut( 'Оставить уникальные элементы массива',
+      exTwentyThree.toString(),
+      unique(strings),
+      'https://learn.javascript.ru/task/array-unique'
   );
 }
