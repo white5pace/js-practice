97919ac1250c2e17b1307a8fd700deebb1d49782
white5pace <kuchmin_roman@mail.ru>
2019-09-16 12:16:56 +0300

    3.5.3 author solution

--- learn-javascript/DOM_3/script.js
+++ learn-javascript/DOM_3/script.js
@@ -465,6 +465,30 @@ function exNine() {
   //     nextVisible++;
   //   }
   // }
+
+  function isVisible(elem) {
+    let coords = elem.getBoundingClientRect();
+    let windowHeight = document.documentElement.clientHeight;
+    let topVisible = coords.top > 0 && coords.top < windowHeight;
+    let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+
+    return topVisible || bottomVisible;
+  }
+
+  function showVisible() {
+    for (let img of exBody.querySelectorAll('img')) {
+      let realSrc = img.dataset.src;
+      if (!realSrc) continue;
+
+      if (isVisible(img)) {
+        img.src = realSrc;
+        img.dataset.src = '';
+      }
+    }
+  }
+
+  showVisible();
+  window.onscroll = showVisible;
   function execution() {}
   htmlOut(exCode,
       exNine.toString(),
