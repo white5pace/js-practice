99b9c49485638cf768d2901992cfaa7c4a68041c
white5pace <kuchmin_roman@mail.ru>
2019-09-17 12:38:53 +0300

    sass introduction

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp         = require('gulp'),
-    browserSync  = require('browser-sync');
+    sass         = require('gulp-sass'),
+    browserSync  = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('browser-sync', function(){
   browserSync({
@@ -10,9 +12,19 @@ gulp.task('browser-sync', function(){
   })
 });
 
+gulp.task('sass', function(){
+  return gulp.src('learn-javascript/sass/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer(['last 15 versions']))
+    .pipe(gulp.dest('learn-javascript/styles'))
+    .pipe(browserSync.stream())
+});
+
 gulp.task('watch', function(){
   gulp.watch("learn-javascript/**/*.html").on('change', browserSync.reload);
   gulp.watch("learn-javascript/**/*.js").on('change', browserSync.reload);
   gulp.watch("learn-javascript/**/*.css").on('change', browserSync.reload);
+  gulp.watch('learn-javascript/sass/*.scss', gulp.parallel('sass'));
 });
+
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
