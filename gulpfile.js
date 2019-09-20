13cf47d17399e05f47938dba3de44a410a128815
white5pace <kuchmin_roman@mail.ru>
2019-09-20 16:56:26 +0300

    init chapter 11, pug integrate

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     browserSync  = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    pug          = require('gulp-pug');
 
 gulp.task('browser-sync', function(){
   browserSync({
@@ -20,11 +21,22 @@ gulp.task('sass', function(){
     .pipe(browserSync.stream())
 });
 
+gulp.task('pug', function() {
+  return gulp.src(['./learn-javascript/**/*.pug', '!learn-javascript/components/*.pug'])
+    .pipe(pug({
+      pretty: true,
+    }))
+    .pipe(gulp.dest(function (file) {
+        return file.base;
+    }));
+});
+
 gulp.task('watch', function(){
   gulp.watch("learn-javascript/**/*.html").on('change', browserSync.reload);
   gulp.watch("learn-javascript/**/*.js").on('change', browserSync.reload);
   gulp.watch("learn-javascript/**/*.css").on('change', browserSync.reload);
   gulp.watch('learn-javascript/sass/*.scss', gulp.parallel('sass'));
+  gulp.watch('learn-javascript/**/*.pug', gulp.parallel('pug'));
 });
 
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
---
+++ learn-javascript/UJS_11/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function htmlOut(exCode, pOne, execution) {
+  exCode.previousElementSibling.onclick = function() {
+    this.style.display = 'none';
+    exCode.style.display = 'block';
+    exCode.innerHTML = pOne;
+    exCode.parentElement.style.justifyContent = 'start';
+    Prism.highlightElement(exCode);
+    execution();
+  };
+};
+
+function elevenOneOne() {
+  let ex = document.getElementById('11.1.1');
+  let exBody = ex.querySelector('.ex-body');
+  let exCode = ex.querySelector('.ex-code');
+
+  exBody.querySelector('button').onclick = function() {
+    showCircle(0, 0, 100);
+  };
+
+  let circle = null;
+  function showCircle(cx, cy, radius) {
+    if (circle) return;
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    exBody.append(div);
+    circle = div;
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+    }, 0);
+  }
+
+  function execution() {}
+  htmlOut(exCode,
+      elevenOneOne.toString(),
+      execution);
+}
+elevenOneOne();
