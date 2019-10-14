682a2a0cef38845b34a105747d8b797e1fd37798
white5pace <kuchmin_roman@mail.ru>
2019-10-14 17:39:18 +0300

    many changes, i forgot about git

--- learn-javascript/OtherJS_7/script.js
+++ learn-javascript/OtherJS_7/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 window.onload = function() {
@@ -15,5 +16,25 @@ window.onload = function() {
 
 
 function sevenSixOne() {
-  console.log('kus');
+  let str = 'Завтрак в 09:00 в комнате 123:456';
+  alert(str.match(/\b\d\d:\d\d\b/));
+}
+
+
+function sevenEightTwo() {
+  let regexp = /\b\d\d[:-]\d\d\b/g;
+  alert( 'Завтрак в 09:00. Ужин в 21-30'.match(regexp) ); // 09:00, 21-30
+}
+
+function sevenNineOne() {
+  let regexp = /\.+/g;
+  alert( 'Привет!... Как дела?.....'.match(regexp) ); // ..., .....
+}
+
+function sevenNineTwo() {
+  let regexp = /#[0-9a-f]{6}\b/gi;
+
+  let str = 'color:#121212; background-color:#AA00ef bad-colors:f#fddee #fd2 #12345678';
+
+  alert( str.match(regexp) ); // #121212,#AA00ef
 }
