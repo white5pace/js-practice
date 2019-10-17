02c899701cb9913bc34760e3e49b6bd04d90f1e6
white5pace <kuchmin_roman@mail.ru>
2019-10-17 18:50:19 +0300

    7.11

--- learn-javascript/OtherJS_7/script.js
+++ learn-javascript/OtherJS_7/script.js
@@ -56,3 +56,47 @@ function sevenTenThree() {
 
   alert( str.match(regexp) ); // '<a href="/">', '<input type="radio" checked>', '<b>'
 }
+
+function sevenElevenOne() {
+  let regexp = /#([0-9a-f]{3}){1,2}\b/gi;
+
+  let str = 'color: #3f3; background-color: #AA00ef; and: #abcd';
+
+  alert( str.match(regexp) ); // #3f3 #AA00ef
+}
+
+function sevenElevenTwo() {
+  let regexp = /-?\d+(\.\d+)?/g;
+
+  let str = '-1.5 0 2 -123.4.';
+
+  console.log( str.match(regexp) ); // -1.5, 0, 2, -123.4
+}
+
+function sevenElevenThree() {
+  function parse(exp) {
+    let regexp = /(\d+\.?\d*)\s*([-+*\/])\s*(\d+\.?\d*)/;
+    // Но можно было и так (\d(?:\.\d+)?)
+    let result = exp.match(regexp);
+    if (!result) return [];
+    result.shift();
+    return result;
+  }
+  let [a, op, b] = parse('1.2 * 3.4');
+
+  alert(a); // 1.2
+  alert(op); // *
+  alert(b); // 3.4
+}
+
+function sevenElevenFour() {
+  let regexp = /([0-9a-f]{2}:){5}[0-9a-f]{2}/gi;
+
+  alert( regexp.test('01:32:54:67:89:AB') ); // true
+
+  alert( regexp.test('0132546789AB') ); // false (нет двоеточий)
+
+  alert( regexp.test('01:32:54:67:89') ); // false (5 чисел, должно быть 6)
+
+  alert( regexp.test('01:32:54:67:89:ZZ') ) // false (ZZ в конце строки)
+}
