e4c019ce68d3634b5f46109df4996b2056538a61
white5pace <kuchmin_roman@mail.ru>
2020-11-29 23:57:58 +0300

    react-quiz forms, server. Redux begin :file_folder:

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,10 +1,142 @@
 import React, {Component} from 'react';
+import classes from './Auth.module.css'
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
+import {validate, validateForm} from "../../form/formFramework";
+import axios from 'axios'
+
+const apiKey = 'AIzaSyCPEwPySOT5Qxo6l2ekx1bDWOwM4nka8Es'
 
 class Auth extends Component {
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Enter correct email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Password',
+        errorMessage: 'Enter correct password',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+
+  singInHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+
+    try {
+      const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, authData)
+
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  singUpHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+
+    try {
+      const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, authData)
+
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
+  onChangeHandler = (event, controlName) => {
+    const formControls = { ...this.state.formControls}
+    const control = { ...formControls[controlName] }
+
+    control.value = event.target.value
+    control.touched = true
+    control.valid = validate(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls,
+      isFormValid: validateForm(formControls)
+    })
+  }
+
+  renderInputs() {
+    return  Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+        )
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Authorization</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+
+            { this.renderInputs()}
+
+            <Button
+              type="success"
+              onClick={this.singInHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Sing in
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.singUpHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Sing up
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
--- react-quiz/src/containers/Quiz/Quiz.js
+++ react-quiz/src/containers/Quiz/Quiz.js
@@ -2,38 +2,17 @@ import React, {Component} from 'react'
 import classes from './Quiz.module.css'
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
+import axios from '../../axios/axios-quiz'
+import Loader from "../../components/UI/Loader/Loader";
 
 class Quiz extends Component {
   state = {
     isFinished: false,
-    results: {}, /* {[id] ? 'success : error} */
+    results: {},
     activeQuestion: 0,
-    answerState: null, /* {[id] ? 'success' : 'error' } */
-    quiz: [
-      {
-        question: 'What color is the sky?',
-        rightAnswerId: 2,
-        id: 1,
-        answers: [
-          {text: 'Black', id: 1},
-          {text: 'Blue', id: 2},
-          {text: 'Red', id: 3},
-          {text: 'Green', id: 4}
-        ]
-      },
-      {
-        question: 'When was St. Petersburg founded?',
-        rightAnswerId: 3,
-        id: 2,
-        answers: [
-          {text: '1700', id: 1},
-          {text: '1702', id: 2},
-          {text: '1703', id: 3},
-          {text: '1803', id: 4}
-        ]
-      }
-    ],
-
+    answerState: null,
+    quiz: [],
+    loading: true,
   }
 
   onAnswerClickHandler = (answerId) => {
@@ -94,8 +73,17 @@ class Quiz extends Component {
     })
   }
 
-  componentDidMount() {
-    console.log('Quiz ID = ', this.props.match.params.id)
+  async componentDidMount() {
+    try {
+      const response = await axios.get(`/quizes/${this.props.match.params.id}.json`)
+      const quiz = response.data
+      this.setState({
+        quiz,
+        loading: false
+      });
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   render() {
@@ -103,21 +91,26 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Answer all questions</h1>
-          {this.state.isFinished ?
-            <FinishedQuiz
-              results = {this.state.results}
-              quiz = {this.state.quiz}
-              onRetry = {this.retryHandler}
-            /> :
-            <ActiveQuiz
-              answers={this.state.quiz[this.state.activeQuestion].answers}
-              question={this.state.quiz[this.state.activeQuestion].question}
-              onAnswerClick={this.onAnswerClickHandler}
-              quizLength={this.state.quiz.length}
-              answerNubmer={this.state.activeQuestion + 1}
-              answerState={this.state.answerState}
-            />
+
+          {
+            this.state.loading
+              ? <Loader/>
+              : this.state.isFinished ?
+              <FinishedQuiz
+                results = {this.state.results}
+                quiz = {this.state.quiz}
+                onRetry = {this.retryHandler}
+              /> :
+              <ActiveQuiz
+                answers={this.state.quiz[this.state.activeQuestion].answers}
+                question={this.state.quiz[this.state.activeQuestion].question}
+                onAnswerClick={this.onAnswerClickHandler}
+                quizLength={this.state.quiz.length}
+                answerNubmer={this.state.activeQuestion + 1}
+                answerState={this.state.answerState}
+              />
           }
+
         </div>
       </div>
     )
