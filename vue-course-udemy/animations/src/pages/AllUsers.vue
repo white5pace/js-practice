c6ce31729ddfee9b969ab1277a5c124d5119d3ee
white5pace <kuchmin_roman@mail.ru>
2021-03-07 13:21:17 +0300

    :incoming_envelope: vue-course sources migration to standalone rep

--- vue-course-udemy/animations/src/pages/AllUsers.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container">
-    <h2>All Users</h2>
-    <router-link to="goals">Course goals</router-link>
-  </div>
-</template>
--- vue-course-udemy/routing/src/pages/NotFound.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h2>Page not found!</h2>
-</template>
